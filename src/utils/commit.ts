7defb0868bd9666990caa0f5fc3c999e4145e6b8
JSH-data <super60447@gmail.com>
2024-05-28 14:32:18 +0900

    feat: add commit message maker

---
+++ src/utils/commit.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getUpdateCommitMessage(fileName: string, platform: string) {
+  return `Update Algorithm ${fileName} from ${platform}`;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function getUploadCommitMessage(fileName: string, platform: string) {
+  return `Upload Algorithm ${fileName} from ${platform}`;
+}
