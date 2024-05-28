b3dd5aa4834caad493ddbfa830120d4fadbd8cc9
JSH-data <super60447@gmail.com>
2024-05-28 14:30:28 +0900

    chore: add local storage key constant

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const CONSTANT = {
+  REPO_NAME: "repo_name",
+  OWNER_NAME: "owner_name",
+  STORAGE_PATH: "storage_path",
+  ACCESS_TOKEN: "access_token",
+} as const;
