c8bf651f431f13a2c43438eee146783b482a476d
JSH-data <super60447@gmail.com>
2024-05-28 14:31:37 +0900

    feat: add encode util

---
+++ src/utils/parser.ts
@@ -0,0 +1,19 @@
+// Content를 Base64 포멧으로 변환하기 위한 함수입니다.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function contentEncoder(code: string) {
+  console.log("original code", code);
+
+  const encoder = new TextEncoder();
+
+  const bytes = encoder.encode(code);
+  console.log("bytes", bytes);
+
+  const binaryString = String.fromCodePoint(...bytes);
+  console.log("binary string", binaryString);
+
+  const base64EncodedString = btoa(binaryString);
+
+  console.log("After encode", base64EncodedString);
+
+  return base64EncodedString;
+}
